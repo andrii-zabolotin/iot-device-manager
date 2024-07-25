18cf09c5205915990a712106f0b22a7565e806c1
Andrii Zabolotin <fenics7511@gmail.com>
2024-07-25 15:58:04 +0300

    Change variable's name

--- src/database.py
+++ src/database.py
@@ -4,14 +4,13 @@ from peewee import *
 
 db_name = os.getenv('DB_NAME')
 db_user = os.getenv('DB_USER')
-db_password = os.getenv('DB_PASSWORD')
+db_password = os.getenv('DB_PASS')
 db_host = os.getenv('DB_HOST')
-db_port = os.getenv('DB_PORT')
 
 db = PostgresqlDatabase(
     database=db_name,
     user=db_user,
     password=db_password,
     host=db_host,
-    port=db_port
+    port=5432
 )
